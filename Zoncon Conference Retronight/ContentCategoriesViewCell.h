104cb238cf5263e8fa0deea0266ef1b5706f8e2d
hrushi <hrushi@megotechnologies.com>
2016-04-21 00:04:19 +0530

    First Commit

---
+++ Zoncon Conference Retronight/ContentCategoriesViewCell.h
@@ -0,0 +1,17 @@
+//
+//  AbsWorldCollectionViewCell.h
+//  Abs Fitness Express
+//
+//  Created by Hrushikesh  on 11/11/15.
+//  Copyright © 2015 MeGo Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentCategoriesViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) IBOutlet UILabel *textTitle;
+@property (nonatomic, strong) IBOutlet UILabel *textSub;
+@property (nonatomic, strong) IBOutlet UIImageView *imageView;
+
+@end
---
+++ Zoncon Conference Retronight/ContentCategoriesViewCell.m
@@ -0,0 +1,26 @@
+//
+//  AbsWorldCollectionViewCell.m
+//  Abs Fitness Express
+//
+//  Created by Hrushikesh  on 11/11/15.
+//  Copyright © 2015 MeGo Technologies. All rights reserved.
+//
+
+#import "ContentCategoriesViewCell.h"
+
+@implementation ContentCategoriesViewCell
+
+@synthesize textTitle = _textTitle;
+@synthesize textSub = _textSub;
+@synthesize imageView = _imageView;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
